717451263dd419fa8baf5ae61f6ab71297143b81
Markensau <oetkenlukas@gmail.com>
2022-02-19 20:41:41 +0100

    Updating

--- src/webPanel/banuser/banuser.ts
+++ src/webPanel/banuser/banuser.ts
@@ -1,11 +1,27 @@
 import * as express from 'express';
 import * as webPanelConfig from '../../configs/webPanel.json';
 import * as index from '../../index';
+import * as embedHandler from '../../util/embedHandler';
+import * as Discord from 'discord.js';
+import * as botconf from '../../configs/botconfig.json';
 
 export function banUser(req: express.Request, res: express.Response) {
     if (req.body.id != (undefined || null)) {
         index.bot.guilds.cache.map(guild => {
             if (req.body.reason != (undefined || null)) {
+                guild.members.fetch(req.body.id).then(member => {
+                    let embed = new Discord.MessageEmbed()
+                        .setColor('RED')
+                        .setTitle('💥 BAN-SYSTEM 💥')
+                        .setDescription(`Your banned from the Server \n Reason: No reason defined.`)
+                        .setAuthor({ name: `Request by: Webpanel`, iconURL: (botconf.standartPicURL) })
+                        .setFooter(embedHandler.getFooter())
+                        .setTimestamp(new Date())
+                        .setThumbnail((guild.iconURL({dynamic: true}) || botconf.standartPicURL));    
+                    member.send({
+                            embeds: [embed]
+                        }).catch((err) => { })
+                }).catch((err) => { })
                 guild.members.ban(req.body.id, {reason: req.body.reason}).then(() => {
                     res.send({
                         status: 'success',
@@ -19,6 +35,19 @@ export function banUser(req: express.Request, res: express.Response) {
                     });
                 })
             } else {
+                guild.members.fetch(req.body.id).then(member => {
+                    let embed = new Discord.MessageEmbed()
+                        .setColor('RED')
+                        .setTitle('💥 BAN-SYSTEM 💥')
+                        .setDescription(`Your banned from the Server \n Reason: ${req.body.reason}.`)
+                        .setAuthor({ name: `Request by: Webpanel`, iconURL: (botconf.standartPicURL) })
+                        .setFooter(embedHandler.getFooter())
+                        .setTimestamp(new Date())
+                        .setThumbnail((guild.iconURL({dynamic: true}) || botconf.standartPicURL));    
+                    member.send({
+                            embeds: [embed]
+                        }).catch((err) => { })
+                }).catch((err) => { })
                 guild.members.ban(req.body.id, {reason: req.body.reason}).then(() => {
                     res.send({
                         status: 'success',
--- src/webPanel/webPanel.ts
+++ src/webPanel/webPanel.ts
@@ -3,10 +3,12 @@ import * as webPanelConfig from '../configs/webPanel.json';
 import * as index from '../index';
 import * as banHandler from './banuser/banuser';
 import * as changeUserNameHandler from './changeusername/changeusername';
+import * as fs from 'fs';
 const app = express();
 app.use(express.urlencoded( { extended: true } ));
 
 let ipArray: string[] = [];
+
 export function startWebPanel() {
     app.post('/', (req, res) => {
         res.header('X-Powered-By', 'LvckyWorld.net');
@@ -32,6 +34,33 @@ export function startWebPanel() {
         if (req.body.action == 'banuser') {
             banHandler.banUser(req, res);
         }
+    });
+
+    app.get("/userlist", (req, res) => {
+        
+        let json: { id: string; name: string; tag: string; avatarURL: string | null; }[] = [];
+        res.header('X-Powered-By', 'LvckyWorld.net');
+        index.bot.guilds.cache.forEach(guild => {
+            guild.members.cache.forEach(member => {
+                let newJsonObject = {
+                    id: member.id,
+                    name: member.user.username,
+                    tag: member.user.discriminator,
+                    avatarURL: member.user.avatarURL({ dynamic: true }),
+                }
+                json.push(newJsonObject);
+            })
+        })
+
+        res.send(JSON.stringify(json));
+    });
+    app.get("/admins", (req, res) => {
+        fs.readFile(__dirname + '/admins.json', (err, data) => {
+            console.log(err)
+            if (err) return;
+            res.header('X-Powered-By', 'LvckyWorld.net');
+            res.send(data);
+        });
     });
     
     
