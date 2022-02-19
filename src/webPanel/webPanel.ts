74eac452cb1ee96b0714b4198a83f9a6a1a03f8d
Markensau <oetkenlukas@gmail.com>
2022-02-19 16:10:26 +0100

    Updating Security

--- src/webPanel/webPanel.ts
+++ src/webPanel/webPanel.ts
@@ -6,12 +6,27 @@ import * as changeUserNameHandler from './changeusername/changeusername';
 const app = express();
 app.use(express.urlencoded( { extended: true } ));
 
+let ipArray: string[] = [];
+let bannedIPs: string[] = [];
 export function startWebPanel() {
     app.post('/', (req, res) => {
-        if (req.body.action == (undefined || null)) return;
         res.header('X-Powered-By', 'LvckyWorld.net');
-        if (req.headers['verify'] != webPanelConfig.verifyHeader) return res.sendStatus(403);
         
+        let ipCount = ipArray.filter(ip => ip == req.ip).length;
+        // If IP is more than maxRequestsIfIPisNotVerified time's on array
+        if (ipCount >= webPanelConfig.maxRequestsIfIPisNotVerified) {
+            return res.status(403).send("Your IP is banned!");
+        }
+        
+        if (req.body.action == (undefined || null)) return res.status(403).send("Your IP wasn't found");
+
+        if (req.headers['verify'] != webPanelConfig.verifyHeader) {
+            ipArray.push(req.ip);
+            
+            res.status(403).send("You are not allowed to access this page! If you try this again, you will be banned from this API for 1 day!");
+            return;
+        }
+
         if (req.body.action == 'changeusername') {
             changeUserNameHandler.changeUserName(req, res);
         }
